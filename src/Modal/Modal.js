f3aa53740cbffd4d4e7e656047e12fcd930e6892
phamthienduy-dev <duyphamthien@outlook.com>
2021-12-12 21:09:49 +0700

    Fix netlify form

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -27,37 +27,47 @@ function Modal(props) {
           data-netlify="true"
           onSubmit="submit"
         >
+          <input type="hidden" name="form-name" value="review" />
           <div>
-            <label className="font-semibold dark:text-gray-200">
+            <label
+              className="font-semibold dark:text-gray-200"
+              htmlFor="opinion"
+            >
               {vietnamese
                 ? "Bạn nghĩ sao về portfolio của mình ?"
                 : "What do you think about my portfolio?"}
             </label>
             <textarea
+              id="opinion"
               name="opinion"
               className="w-3/4 border-2 border-solid border-gray-500 p-2 rounded-md mt-1 dark:bg-gray-600 dark:text-gray-200"
             />
           </div>
 
           <div className="mt-4">
-            <label className="font-semibold dark:text-gray-200">
+            <label className="font-semibold dark:text-gray-200" html="bugs">
               {vietnamese
                 ? "Bạn có phát hiện ra lỗi? Nếu có hãy chỉ mình nhé!"
                 : "Have you spotted any bugs, please show me!"}
             </label>
             <textarea
+              id="bugs"
               name="bugs"
               className="w-3/4 border-2 border-solid border-gray-500 p-2 rounded-md mt-1 dark:bg-gray-600 dark:text-gray-200"
             />
           </div>
 
           <div className="mt-4">
-            <label className="font-semibold dark:text-gray-200">
+            <label
+              className="font-semibold dark:text-gray-200"
+              htmlFor="improvement"
+            >
               {vietnamese
                 ? "Mình cần làm gì để cải thiệt portfolio này?"
                 : "What should I do to improve this porfolio?"}
             </label>
             <textarea
+              id="improvement"
               name="improvement"
               className="w-3/4 h-64 border-2 border-solid border-gray-500 p-2 rounded-md mt-1 dark:bg-gray-600 dark:text-white"
             />
